33e98ea12d2a9f002eb5994ee0fc45f24ef2fa67
Daiki Murakami <murakami@Murakami-no-MacBook-Air.local>
2016-10-07 15:22:06 +0900

    happy ruby

---
+++ lib/murakami/ex_files.rb
@@ -0,0 +1,4 @@
+require "ex1_1"
+#require "ex2_1"
+#require "ex3_1"
+
--- lib/rubynovice.rb
+++ lib/rubynovice.rb
@@ -1,6 +1,7 @@
 require "rubynovice/version"
 $LOAD_PATH.unshift File.expand_path("../../lib/#{ENV['RUBYNOVICE_NAME']}", __FILE__)
 begin
+  p $LOAD_PATH
   require 'ex_files'
 rescue LoadError
   p "Load Error of ex_files in rubynovice.rb."
